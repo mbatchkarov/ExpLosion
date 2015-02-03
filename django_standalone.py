5df681d555d255bb38bc3572d2bb34c0d9bdedce
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-02-03 12:56:26 +0000

    removed unneeded import that caused problems
    messing with matplotlib configuration in order to get figures ready
    for the webserver shouldnt be done when there isnt a webserver.
    conflicts with seaborn/ipynb configurations

--- django_standalone.py
+++ django_standalone.py
@@ -5,9 +5,8 @@ import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ExpLosion.settings'
 django.setup() # new in django 1.7
 
-# this import needs to happen AFTER django has been configured
-from gui.user_code import *
-
 # some code that uses the django ORM
+# this import needs to happen AFTER django has been configured
+# from gui.user_code import *
 # exp_ids = [37, 43, 49]
 # get_demsar_diagram(*get_demsar_params(exp_ids))
